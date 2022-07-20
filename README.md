b1f67d606848adcfc139e6949a6c3ee6198c4e65
Jianan Liu <jianan1991@huskers.unl.edu>
2022-07-20 15:07:13 -0700

    readme in auth branch 2nd commit
    readme in auth branch
    
    change random email comments
    
    readme in auth branch 3rd commit
    
    readme in auth branch 4th commit

--- README.md
+++ README.md
@@ -41,3 +41,11 @@ ddddd
 - add 2nd line from master
 
 - add 3rd line from master
+
+- Auth branch
+
+- auth branch 2nd commit
+
+- auth branch 3rd commit
+- 
+- auth branch 4th commit
--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -9,8 +9,9 @@ import (
 )
 
 func CreateRandomAccount(t *testing.T) Account {
+	user := CreateRandomUser(t)
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
